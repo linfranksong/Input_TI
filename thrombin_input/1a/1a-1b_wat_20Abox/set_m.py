b123e1ed3bbbac81e211fdd5cf2e70ff8e43e3ee
Lin Frank Song <songlin3@msu.edu>
2019-04-09 20:03:47 -0400

    thrombin

---
+++ thrombin_input/1a/1a-1b_wat_20Abox/set_m.py
@@ -0,0 +1,33 @@
+import os
+
+dir = '/mnt/scratch/songlin3/run/thrombin/ligand_final/final/1a_resp/wat_20Abox/ti_one-step/1a_1b/'
+filesdir = dir + 'files/'
+temp_equiin = filesdir + 'temp_equi_m.in'
+temp_prodin = filesdir + 'temp_prod.in'
+temp_m.pbs = filesdir + 'temp_m.pbs'
+
+lambd = [ 0.00922, 0.04794, 0.11505, 0.20634, 0.31608, 0.43738, 0.56262, 0.68392, 0.79366, 0.88495, 0.95206, 0.99078]
+for j in lambd:
+  os.system("rm -r %6.5f" %(j)) 
+  os.system("mkdir %6.5f" %(j)) 
+  os.chdir("%6.5f" %(j))
+  os.system("rm *")
+  workdir = dir + "%6.5f" %(j) + '/'
+  #equiin
+  eqin = workdir + "%6.5f_equi_m.in" %(j)
+  os.system("cp %s %s" %(temp_equiin, eqin))
+  os.system("sed -i 's/XXX/%6.5f/g' %s" %(j, eqin))
+  #prodin
+  prodin = workdir + "%6.5f_prod.in" %(j)
+  os.system("cp %s %s" %(temp_prodin, prodin))
+  os.system("sed -i 's/XXX/%6.5f/g' %s" %(j, prodin))
+  #PBS
+  pbs = workdir + "%6.5f.pbs" %(j)
+  os.system("cp %s %s" %(temp_m.pbs, pbs))
+  os.system("sed -i 's/XXX/%6.5f/g' %s" %(j, pbs))
+  #top
+  os.system("cp ../1a-1b_merged.prmtop .")
+  os.system("cp ../0.5_equi_0_3.rst .")
+  #submit pbs 
+  os.system("qsub %s" %(pbs)) 
+  os.chdir(dir)
